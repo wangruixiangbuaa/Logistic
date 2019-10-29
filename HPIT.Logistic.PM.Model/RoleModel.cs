366757418641b310206d482de05bd4508965fa90
wangruixiangbuaa <627730788@qq.com>
2019-10-29 11:16:31 +0800

    添加文件

---
+++ HPIT.Logistic.PM.Model/RoleModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPIT.Logistic.PM.Model
+{
+    public class RoleModel
+    {
+        public int RoleID { get; set; }
+        public string RoleName { get; set; }
+        public string RolePurview { get; set; }
+    }
+}
